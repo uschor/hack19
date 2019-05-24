050c96c16743a985c586eee7273a90a3c8c388ac
jonathan <jonathan@tenisoft.com>
2019-05-24 08:37:27 +0300

    batch file

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
+@cd %~dp0
 @"java.exe" -Dfile.encoding=UTF-8 -classpath "bin;ext-jars\lucene-analyzers-common-8.1.0.jar;ext-jars\lucene-core-8.1.0.jar;ext-jars\lucene-queryparser-8.1.0.jar" gniza.Main
